48ac4f90f805c25f9ea5b6bba410d84abd1abd5a
RayCatYT <raycatyt@gmail.com>
2022-10-08 20:22:08 +0200

    Only ME

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def chatter(resp):
     if resp.event.message:
         message = resp.parsed.auto()
         if message['content'].startswith('c.'):
-            with cleverbotfree.sync_playwright() as p_w:
-                c_b = cleverbotfree.Cleverbot(p_w)
-                user_input = message['content'].split("c.")[1]
-                answer = c_b.single_exchange(user_input)
-                bot.sendMessage(message['channel_id'], answer)
+            username = message['author']['username']
+            if username == "Azazel":
+                with cleverbotfree.sync_playwright() as p_w:
+                    c_b = cleverbotfree.Cleverbot(p_w)
+                    user_input = message['content'].split("c.")[1]
+                    answer = c_b.single_exchange(user_input)
+                    bot.sendMessage(message['channel_id'], answer)
